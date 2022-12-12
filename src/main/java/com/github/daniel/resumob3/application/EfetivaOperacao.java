c939acfdf9e57dbcdc2edd96f391fa1b336917d9
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-12-12 15:59:39 -0300

    Update

---
+++ src/main/java/com/github/daniel/resumob3/application/EfetivaOperacao.java
@@ -0,0 +1,73 @@
+package com.github.daniel.resumob3.application;
+
+import com.github.daniel.resumob3.domain.*;
+import com.github.daniel.resumob3.io.CSVResumoRepository;
+import com.github.daniel.resumob3.io.XLSXNegociacaoRepository;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class EfetivaOperacao extends Component {
+    void efetivaOperacao(){
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Selecione o arquivo com as transações");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Arquivos execel", "xlsx");
+        fileChooser.setFileFilter(filter);
+
+        int retorno = fileChooser.showOpenDialog(this);
+
+        File file;
+        if (retorno == JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile();
+        }
+        else {
+            return;
+        }
+        String pathParent = file.getParent();
+        String pathResumo = String.valueOf(Path.of( pathParent, "resumo.csv"));
+
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            fileOutputStream = new FileOutputStream(pathResumo);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        NegociacaoRepository negociacaoRepo = new XLSXNegociacaoRepository();
+
+        FileInputStream fileInputStream;
+
+        {
+            try {
+                fileInputStream = new FileInputStream(file);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        List<Negociacao> todas = negociacaoRepo.load(fileInputStream);
+        List<String> ativos = ComputoDosDados.getAtivos(todas);
+
+        Resumo resumo = new Resumo();
+        for (String ativo : ativos) {
+            Negociacao resumoNegociacao = ComputoDosDados.resumoNegocicoes(todas, ativo);
+            resumo.addResumoNegociacoes(resumoNegociacao);
+        }
+
+        ResumoRepository resumoRepo = new CSVResumoRepository();
+        resumoRepo.save(resumo, fileOutputStream);
+    }
+}
--- src/main/java/com/github/daniel/resumob3/application/Main.java
+++ src/main/java/com/github/daniel/resumob3/application/Main.java
@@ -1,12 +1,14 @@
 package com.github.daniel.resumob3.application;
 
-public class Aplicacao {
+public class Main {
     public static void main(String[] args) {
-        System.exit(start(args));
-         }
-        static int start(Strings[] args) {
+        System.exit(start());
+    }
 
-            return 0;
+    static int start() {
+        EfetivaOperacao efetiva = new EfetivaOperacao();
 
+        efetiva.efetivaOperacao();
+        return 0;
     }
 }
