79a3339688652454d84f504e5f2b6a2a4b5528d5
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-12-12 15:58:03 -0300

    Front

---
+++ src/main/java/com/github/daniel/resumob3/application/Main.java
@@ -0,0 +1,12 @@
+package com.github.daniel.resumob3.application;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        System.exit(start(args));
+         }
+        static int start(Strings[] args) {
+
+            return 0;
+
+    }
+}
--- src/main/java/com/github/daniel/resumob3/application/console/Aplicacao.java
+++
@@ -1,6 +0,0 @@
-package com.github.daniel.resumob3.application.console;
-
-public class Aplicacao {
-    public static void main(String[] args) {
-    }
-}
