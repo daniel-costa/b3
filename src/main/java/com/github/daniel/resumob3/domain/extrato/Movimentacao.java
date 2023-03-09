4c1f81493d7fe91476a8a5003c9bd51519411e1a
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-03-09 11:54:54 -0300

    Classe movimentacao

---
+++ src/main/java/com/github/daniel/resumob3/domain/extrato/Movimentacao.java
@@ -0,0 +1,33 @@
+package com.github.daniel.resumob3.domain.extrato;
+
+import java.util.Date;
+
+public class Movimentacao {
+    private Date data;
+    private String descricao;
+    private double valor;
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
